0d61c6430fbd5db09c5fd27a91c541538f75d4ab
Shewone2004 <134802138+Shewone2004@users.noreply.github.com>
2023-06-08 22:22:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ const chalk = require('chalk')
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OFFLINE = false // if you want put bot offline put true
-global.BOT_PRIVET = false // if you want use bot privet 
+global.BOT_PRIVET = true // if you want use bot privet 
 global.AUTO_CHATGPT = false // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
@@ -16,9 +16,9 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
+global.OWNER_NAME = '𝙄 𝘼𝙢 𝘾𝙝𝙖𝙩𝙂𝙥𝙩 𝘽𝙤𝙩 🫶🏻' // Owner Name
+global.OWNER_NUMBER = ['94775434030'] //Owner Number
+global.BOT_ADMINS = ['94775434030','94775434030'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 
 
@@ -27,14 +27,14 @@ global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and 
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
+global.AI_PASSWORD = 'king-nima-gpt-bot'
 
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
 */
-global.CMD1 = '' // .ai Command
-global.CMD2 = '' // .ai2 Command
+global.CMD1 = 'gpt' // .ai Command
+global.CMD2 = 'chtgpt' // .ai2 Command
 global.PORT = process.env.PORT || '5000' // Port No need Change.
 
 
